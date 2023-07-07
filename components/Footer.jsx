918ab9feb92afe92b787eae1929f7f29a5cd9de8
Leticia <leticiaschaves7@gmail.com>
2023-07-06 21:04:49 -0300

    feat: adiciona componentes Header e Footer com HTML básico

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <nav>
+        <a href="https://www.instagram.com/InstitutoAristoteles">Instagram</a>
+        <a href="https://www.facebook.com/InstitutoAristoteles">Facebook</a>
+        <a href="https://www.twitter.com/InstitutoAristoteles">Twitter</a>
+      </nav>
+      <h3>Copyright 2023, Instituto Aristóteles</h3>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Instituto Aristóteles</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Início</a></li>
+          <li><a href="/nosotros">Quem somos</a></li>
+          <li><a href="/contacto">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
