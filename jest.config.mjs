78b6f41e8a16ab85af6d45c99cfa3dbe7727a264
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-07-09 19:27:17 -0300

    adiciona métodos de integração com notion

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,13 +8,14 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
-  // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
+    /* react: 'next/dist/compiled/react/cjs/react.development.js', */
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
+  testEnvironmentOptions: {}
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
