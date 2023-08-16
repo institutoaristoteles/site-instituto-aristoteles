80377b4bfb866a978934972f3c151489575898b8
Mylena <mylenaleiteb@gmail.com>
2023-08-15 22:41:42 -0300

    alteração das aspas do arquivo de configuração do jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,4 +1,4 @@
-import nextJest from "next/jest.js";
+import nextJest from "next/jest.js"
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -8,14 +8,14 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     /* react: 'next/dist/compiled/react/cjs/react.development.js', */
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  testEnvironmentOptions: {}
+  testEnvironmentOptions: {},
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
