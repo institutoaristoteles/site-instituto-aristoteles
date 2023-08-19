33f4023650ae10fc4d67705ec3ec9b87e2ed37e4
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-08-19 01:18:27 -0300

    ajusta componente de badge do autor

--- src/shared/components/post-author-badge.tsx
+++ src/shared/components/post-author-badge.tsx
@@ -1,17 +1,41 @@
+"use client"
+
 import Image from "next/image"
-import { memo, use } from "react"
-import { Author } from "@/integration/types"
+import { memo } from "react"
 import { fetchAuthorById } from "@/integration/api"
+import useSWR from "swr"
+import Skeleton from "react-loading-skeleton"
+
+const PostAuthorSkeleton = () => (
+  <div className="flex gap-2 items-center">
+    <Skeleton circle height={24} width={24} />
+    <span className="text-xs">
+      <Skeleton width={100} height={12} />
+    </span>
+  </div>
+)
+
+const authorFetcher = async (authorId: string) => {
+  return await fetchAuthorById(authorId)
+}
 
 function PostAuthorBadge({ authorId }: { authorId: string }) {
-  const author = use<Author>(fetchAuthorById(authorId))
+  const { isLoading, data: author } = useSWR(authorId, authorFetcher)
+
+  if (isLoading) {
+    return <PostAuthorSkeleton />
+  }
+
+  if (!author) {
+    return <></>
+  }
 
   return (
     <div className="flex gap-2 items-center">
       {author.avatar && (
         <Image
           src={author.avatar}
-          alt=""
+          alt={`Avatar de ${author.name}`}
           width={40}
           height={40}
           className="rounded-full w-7 shadow"
