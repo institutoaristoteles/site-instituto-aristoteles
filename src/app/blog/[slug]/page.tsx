85850a4a0e4036e2a1d141f49bd4d985d8d52c5a
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-09-06 19:09:13 -0300

    carrega conteúdo do post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getPostBySlug } from "@/integration/notion"
+import { getPostBySlug, getPostContent } from "@/integration/notion"
 import Image from "next/image"
 import { notFound } from "next/navigation"
 
@@ -7,13 +7,29 @@ export default async function PostPage(props: { params: { slug: string } }) {
   if (!post) {
     return notFound()
   }
+
+  const content = await getPostContent(post.id)
+
   return (
-    <main>
-      <div className="container">
+    <article>
+      <div className="container flex flex-col gap-5">
         {post.image && (
-          <Image src={post.image} alt="" width={200} height={200} />
+          <Image
+            src={post.image}
+            alt=""
+            width={3000}
+            height={3000}
+            className="max-h-[250px] object-cover object-center full-width"
+          />
         )}
+
+        <header>
+          <h1>{post.title}</h1>
+          <div>{post.description}</div>
+        </header>
+
+        <div className="prose" dangerouslySetInnerHTML={{ __html: content }} />
       </div>
-    </main>
+    </article>
   )
 }
